bf391ca3a6b6f4f924be8ae1f0ead11a93652a1b
AseelAbdullatif <abdulatifaseel90@gmail.com>
2024-07-31 18:16:28 +0300

    restoarant_api

--- src/app/constant/Routes.ts
+++ src/app/constant/Routes.ts
@@ -25,3 +25,18 @@ Product_By_Type : BASEURL+'product-type',
   // Orders_every_month : BASEURL+'show/',
 
 }
+
+
+
+
+export const  rest_orders={
+  //اضافة منتج في الطلب
+  New_Product : BASEURL+'addProduct',
+  //تفاصيل الطلب
+  View_single_request_With_products : BASEURL+'request-products/1',
+  //تعرض كل المنتجات في السستم
+  View_products : BASEURL+'getProducts',
+  View_requests :  BASEURL+'getProducts',
+}
+
+
---
+++ src/app/services/error-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { request } from 'http';
+import { Observable, catchError, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorInterceptorService {
+
+  constructor() { }
+
+intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  return next.handle(request).pipe(catchError(err => {
+    if (err.status === 400) {}
+    if (err.status === 401) {}
+    if (err.status === 403) {}
+    if (err.status === 422) {}
+    if (err.status === 429) {}
+    if (err.status === 500) {}
+    return throwError(err);
+  }))
+}
+}
